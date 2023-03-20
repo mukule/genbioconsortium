9109f8438aa2c64f53a7adfaebad20f1c7bc31cc
mukule <nelsonmasibo6@gmail.com>
2023-03-20 16:43:03 +0300

    let it work

--- genbioconsortium/settings.py
+++ genbioconsortium/settings.py
@@ -32,7 +32,9 @@ import os
 DEBUG = False
 
 
+
 ALLOWED_HOSTS = ['102.216.119.2', 'python.kenyaweb.com']
+
 
 
 
@@ -49,9 +51,7 @@ INSTALLED_APPS = [
     #add the user app
     'users',
     'crispy_forms',
-    'crispy_bootstrap4',
     'django_daraja',
-    #'django_daraja',
     'payment',
     'django_bootstrap_icons',
     'captcha',
@@ -141,20 +141,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL='/static/'
-# STATIC_ROOT=os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT=os.path.join(BASE_DIR, 'static/')
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media/')
-#Remove this line before production
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/'),
-]
+
+
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
