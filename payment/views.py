0b398e3f48c183a3740fe7569966c23bb4b3cddb
mukule <nelsonmasibo6@gmail.com>
2023-05-09 11:11:29 +0300

    reinitialized git repo

--- payment/views.py
+++ payment/views.py
@@ -66,7 +66,7 @@ def create_paypal_order(request):
             {
                 "amount": {
                     "currency_code": "USD",
-                    "value": str(membership_reg.membership_price)
+                    "value": str(membership_reg.membership_price) # <-- changed this
                 },
                 "reference_id": str(membership_reg.id)
             }
@@ -76,12 +76,35 @@ def create_paypal_order(request):
         "Content-Type": "application/json",
         "Authorization": f"Bearer {access_token}"
     }
-    print(webhook_url)
-    print("return_url:", return_url)
-    print("cancel_url:", cancel_url)
     response = requests.post(url, json=payload, headers=headers)
     order = response.json()
+    print(order)
     return JsonResponse(order)
+
+
+@csrf_exempt
+def payment_done(request):
+    order_id = request.POST.get('orderID')
+    print(order_id)
+    access_token = generate_access_token()
+    url = f"{SANDBOX_BASE_URL}/v2/checkout/orders/{order_id}/capture"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {access_token}"
+    }
+    payload = {}
+    response = requests.post(url, json=payload, headers=headers)
+    capture = response.json()
+    print(capture)
+    if capture['status'] == 'COMPLETED':
+        # Handle the case when the payment was successfully completed
+        # e.g., update the user's membership status, send a confirmation email, etc.
+        return render(request, 'payment/payment_done.html')
+    else:
+        # Handle the case when the payment was canceled or failed
+        # e.g., show an error message to the user, log the failure, etc.
+        return render(request, 'payment/payment_canceled.html')
+
 
 
 
@@ -90,11 +113,6 @@ def paypal_webhook(request):
     print('we are here')
     print(request.body)
     return HttpResponse(status=200)
-
-
-@csrf_exempt
-def payment_done(request):
-    return render(request, 'payment/payment_success.html')
 
 
 # Your code here
